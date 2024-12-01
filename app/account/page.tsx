34dbe55c9d59f9d50b0f1ef571bc611d51eeac86
Jinwei Law <lawjingw@gmail.com>
2024-12-01 16:29:56 +0000

    Refactor components to use Lucide icons, improve styling consistency, and add account and reservations pages

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -5,7 +5,11 @@ export const metadata: Metadata = {
 };
 
 function Page() {
-  return <div>Account page</div>;
+  return (
+    <div>
+      <h2 className="mb-7 text-2xl font-semibold text-accent-400">Account</h2>
+    </div>
+  );
 }
 
 export default Page;
