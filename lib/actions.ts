f53a9e7865e54540eb6d215879ef7469b72788d6
Jinwei Law <lawjingw@gmail.com>
2024-12-19 16:47:00 +0000

    Update login redirect path; direct users to account page after login

--- lib/actions.ts
+++ lib/actions.ts
@@ -36,7 +36,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath("/", "layout");
-  redirect("/");
+  redirect("/account");
 }
 
 export async function signup(formData: FormData) {
