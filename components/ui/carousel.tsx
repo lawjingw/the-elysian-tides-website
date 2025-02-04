fb3d1ab11101abc5a9fb631ea2719b5d883d8bcf
Jinwei Law <lawjingw@gmail.com>
2025-02-04 22:34:36 +0000

    Increase button size in carousel component for improved usability; adjust icon sizes to fill the button area

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -206,7 +206,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full",
+        "absolute h-10 w-10 rounded-full",
         orientation === "horizontal"
           ? "left-6 top-1/2 -translate-y-1/2 border-none bg-zinc-800/50 text-white hover:bg-zinc-800/90 hover:text-white"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -216,7 +216,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ChevronLeft className="h-4 w-4" />
+      <ChevronLeft className="h-full w-full" />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
@@ -235,7 +235,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full border-none bg-zinc-800/50 text-white hover:bg-zinc-800/90 hover:text-white",
+        "absolute h-10 w-10 rounded-full border-none bg-zinc-800/50 text-white hover:bg-zinc-800/90 hover:text-white",
         orientation === "horizontal"
           ? "right-6 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -245,7 +245,7 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <ChevronRight className="h-4 w-4" />
+      <ChevronRight className="h-full w-full" />
       <span className="sr-only">Next slide</span>
     </Button>
   );
